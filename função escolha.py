8dc8c6ccbe7aa9a44e739ba681aaf043448146f5
TPD01 <119019018+TPD01@users.noreply.github.com>
2023-07-16 20:22:11 -0300

    Update função escolha.py

--- função escolha.py
+++ função escolha.py
@@ -1,19 +1,27 @@
-def escolha_afnd(a1, a2):
-    estado_inicial = "s"
-    estado_final = "f"
+def uniao_afnd(a1, a2):
+    estados_iniciais = ['s0']  # New initial state
+    estados_finais = ['f0']  # New final state
 
-    estados_finais = [estado_final]
+    novo_estado = 's1'  # New state for transition between a1 and a2
+
+    conjunto_estados = a1.conjunto_estados + a2.conjunto_estados
+    conjunto_estados.remove(a1.estados_finais[0])
+    conjunto_estados.remove(a2.estados_finais[0])
+    conjunto_estados += [novo_estado]  # Add new state
 
     transicoes = {}
-    transicoes[estado_inicial] = {}
-    transicoes[estado_inicial][None] = [a1.estados_iniciais[0], a2.estados_iniciais[0]]
+    transicoes[novo_estado] = {'ε': [a1.estados_iniciais[0], a2.estados_iniciais[0]]}  # ε-transition from new state to initial states of a1 and a2
 
-    for estado in a1.estados_finais:
-        if estado in a1.transicoes:
-            transicoes[estado][None] = [estado_final]
+    for estado, transicoes_estado in a1.transicoes.items():
+        transicoes[estado] = transicoes_estado.copy()
 
-    for estado in a2.estados_finais:
-        if estado in a2.transicoes:
-            transicoes[estado][None] = [estado_final]
+    for estado, transicoes_estado in a2.transicoes.items():
+        if estado in transicoes:
+            transicoes[estado].update(transicoes_estado)
+        else:
+            transicoes[estado] = transicoes_estado.copy()
 
-    return AFND([estado_inicial], estados_finais, transicoes)
+    transicoes[a1.estados_finais[0]] = {'ε': [estados_finais[0]]}  # ε-transition from final state of a1 to new final state
+    transicoes[a2.estados_finais[0]] = {'ε': [estados_finais[0]]}  # ε-transition from final state of a2 to new final state
+
+    return AFND(estados_iniciais, conjunto_estados, estados_finais, transicoes)
