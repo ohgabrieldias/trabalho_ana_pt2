b38995a0edbc94372d29a6382a24d6d0c782635b
TPD01 <119019018+TPD01@users.noreply.github.com>
2023-07-16 20:24:48 -0300

    Update função escolha.py

--- função escolha.py
+++ função escolha.py
@@ -1,16 +1,16 @@
 def uniao_afnd(a1, a2):
-    estados_iniciais = ['s0']  # New initial state
-    estados_finais = ['f0']  # New final state
+    estados_iniciais = ['s0']  # Novo estado inicial
+    estados_finais = ['f0']  # Novo estado final
 
-    novo_estado = 's1'  # New state for transition between a1 and a2
+    novo_estado = 's1'  # Novo estado para transição entre a1 e a2
 
     conjunto_estados = a1.conjunto_estados + a2.conjunto_estados
     conjunto_estados.remove(a1.estados_finais[0])
     conjunto_estados.remove(a2.estados_finais[0])
-    conjunto_estados += [novo_estado]  # Add new state
+    conjunto_estados += [novo_estado]  # adiciona o novo estado
 
     transicoes = {}
-    transicoes[novo_estado] = {'ε': [a1.estados_iniciais[0], a2.estados_iniciais[0]]}  # ε-transition from new state to initial states of a1 and a2
+    transicoes[novo_estado] = {'ε': [a1.estados_iniciais[0], a2.estados_iniciais[0]]}  # ε-transição do novo estado inicial para os estados a1 e a2
 
     for estado, transicoes_estado in a1.transicoes.items():
         transicoes[estado] = transicoes_estado.copy()
@@ -21,7 +21,7 @@ def uniao_afnd(a1, a2):
         else:
             transicoes[estado] = transicoes_estado.copy()
 
-    transicoes[a1.estados_finais[0]] = {'ε': [estados_finais[0]]}  # ε-transition from final state of a1 to new final state
-    transicoes[a2.estados_finais[0]] = {'ε': [estados_finais[0]]}  # ε-transition from final state of a2 to new final state
+    transicoes[a1.estados_finais[0]] = {'ε': [estados_finais[0]]}  # ε-tarnsição do estado final de a1 para o novo estado final
+    transicoes[a2.estados_finais[0]] = {'ε': [estados_finais[0]]}  # ε-tarnsição do estado final de a2 para o novo estado final
 
     return AFND(estados_iniciais, conjunto_estados, estados_finais, transicoes)
