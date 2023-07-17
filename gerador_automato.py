a0d8eaecec1a6f1ed5905d731faaec7b9b63240f
TPD01 <119019018+TPD01@users.noreply.github.com>
2023-07-16 23:11:56 -0300

    Update gerador_automato.py

--- gerador_automato.py
+++ gerador_automato.py
@@ -170,18 +170,32 @@ def concatenacao(a1, a2):
     return AFND(estados_iniciais, conjunto_estados, estados_finais, transicoes)
 ##############################################################################################################
 def uniao_afnd(a1, a2):
-    estado_inicial = "s0"
-    estados_finais = a1.estados_finais + a2.estados_finais
+    estados_iniciais = ['s0']  # Novo estado inicial
+    estados_finais = ['f0']  # Novo estado final
+
+    novo_estado = 's1'  # Novo estado para transição entre a1 e a2
+
+    conjunto_estados = a1.conjunto_estados + a2.conjunto_estados
+    conjunto_estados.remove(a1.estados_finais[0])
+    conjunto_estados.remove(a2.estados_finais[0])
+    conjunto_estados += [novo_estado]  # adiciona o novo estado
 
     transicoes = {}
-    transicoes[estado_inicial] = {}
-    for estado in a1.estados_iniciais:
-        if estado in a1.transicoes:
-            transicoes[estado_inicial].update(a1.transicoes[estado])
+    transicoes[novo_estado] = {'ε': [a1.estados_iniciais[0], a2.estados_iniciais[0]]}  # ε-transição do novo estado inicial para os estados a1 e a2
 
-    transicoes[estado_inicial].update(a2.estados_iniciais)
+    for estado, transicoes_estado in a1.transicoes.items():
+        transicoes[estado] = transicoes_estado.copy()
 
-    return AFND([estado_inicial], estados_finais, transicoes)
+    for estado, transicoes_estado in a2.transicoes.items():
+        if estado in transicoes:
+            transicoes[estado].update(transicoes_estado)
+        else:
+            transicoes[estado] = transicoes_estado.copy()
+
+    transicoes[a1.estados_finais[0]] = {'ε': [estados_finais[0]]}  # ε-tarnsição do estado final de a1 para o novo estado final
+    transicoes[a2.estados_finais[0]] = {'ε': [estados_finais[0]]}  # ε-tarnsição do estado final de a2 para o novo estado final
+
+    return AFND(estados_iniciais, conjunto_estados, estados_finais, transicoes)
 
 
 ##############################################################################################################
