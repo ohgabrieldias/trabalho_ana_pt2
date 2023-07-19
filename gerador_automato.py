9df2878d628fb0e1af93341105e00b1a92ea6eb4
TPD01 <119019018+TPD01@users.noreply.github.com>
2023-07-19 10:00:28 -0300

    Update gerador_automato.py

--- gerador_automato.py
+++ gerador_automato.py
@@ -177,21 +177,19 @@ def concatenacao2(a1, a2):
     return AFND(estados_iniciais, conjunto_estados, estados_finais, transicoes)
 ##############################################################################################################
 def uniao_afnd(a1, a2):
-    estados_iniciais = ['s0']  # Novo estado inicial
-    estados_finais = ['f0']  # Novo estado final
-
-    novo_estado = 's1'  # Novo estado para transição entre a1 e a2
+    novo_estado_inicial = 's0'  # Novo estado inicial
+    estados_finais = a1.estados_finais + a2.estados_finais  # Combine os estados finais de a1 e a2
 
     conjunto_estados = a1.conjunto_estados + a2.conjunto_estados
-    conjunto_estados.remove(a1.estados_finais[0])
-    conjunto_estados.remove(a2.estados_finais[0])
-    conjunto_estados += [novo_estado]  # adiciona o novo estado
 
     transicoes = {}
-    transicoes[novo_estado] = {'ε': [a1.estados_iniciais[0], a2.estados_iniciais[0]]}  # ε-transição do novo estado inicial para os estados a1 e a2
+    transicoes[novo_estado_inicial] = {'ε': [a1.estados_iniciais[0], a2.estados_iniciais[0]]}  # Transições vazias do estado inicial para os iniciais de a1 e a2
 
     for estado, transicoes_estado in a1.transicoes.items():
-        transicoes[estado] = transicoes_estado.copy()
+        if estado in transicoes:
+            transicoes[estado].update(transicoes_estado)
+        else:
+            transicoes[estado] = transicoes_estado.copy()
 
     for estado, transicoes_estado in a2.transicoes.items():
         if estado in transicoes:
@@ -199,10 +197,7 @@ def uniao_afnd(a1, a2):
         else:
             transicoes[estado] = transicoes_estado.copy()
 
-    transicoes[a1.estados_finais[0]] = {'ε': [estados_finais[0]]}  # ε-tarnsição do estado final de a1 para o novo estado final
-    transicoes[a2.estados_finais[0]] = {'ε': [estados_finais[0]]}  # ε-tarnsição do estado final de a2 para o novo estado final
-
-    return AFND(estados_iniciais, conjunto_estados, estados_finais, transicoes)
+    return AFND([novo_estado_inicial], conjunto_estados, estados_finais, transicoes)
 ##############################################################################################################
 def fecho_kleene(a):
     novo_estado = "s0"
