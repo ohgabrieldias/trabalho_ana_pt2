900a992251984612994d4026adab250daefecec7
Gabriel R. Dias <gabrielrd.aluno@unipampa.edu.br>
2023-07-20 05:22:25 -0300

    reafactor: resolving bugs

--- gerador_automato.py
+++ gerador_automato.py
@@ -1,3 +1,7 @@
+# MIT License
+# Copyright (c) [2023] [Gabriel Rosa Dias]
+# See LICENSE file for full license text.
+
 import os
 import random
 import ply.lex as lex
@@ -96,59 +100,11 @@ class AFND:
         self.estados_finais = estados_finais
         self.conjunto_estados = conjunto_estados
         self.transicoes = δ
-##############################################################################################################
-def concatenacaov2(a1, a2):
-    estados_iniciais = a1.estados_iniciais
-    estados_finais = a2.estados_finais
-    novo_estado = a1.estados_finais[0] + a2.estados_iniciais[0]  # Estado inicial de a2 como novo estado
-
-    conjunto_estados =  a1.conjunto_estados + [novo_estado] + a2.conjunto_estados
-    conjunto_estados.remove(a1.estados_finais[0])  # Remove o estado final de a1 do conjunto de estados
-    conjunto_estados.remove(a2.estados_iniciais[0]) # Remove o estado inicial de a2 do conjunto de estados
-
-    transicoes = {}
-    transicoes_tmp = {}
-    transicoes_tmp[novo_estado] = {}
-
-    # Atualiza o nome dos estados finais de a1 para o novo nome
-    for estado in a1.transicoes:
-        for simbolo in a1.transicoes[estado]:
-            if a1.transicoes[estado][simbolo] == [a1.estados_finais[0]]: 
-                a1.transicoes[estado][simbolo] = [novo_estado]
-
-    a1.estados_finais[0] = novo_estado
-
-    for estado in a2.transicoes:
-        for simbolo in a2.transicoes[estado]:
-            if estado == a2.estados_iniciais[0]:
-                transicoes_tmp[novo_estado][simbolo] = [a2.estados_finais[0]]
-                del a2.transicoes[estado][simbolo]
-                break
-
-    a2.transicoes[novo_estado] = transicoes_tmp[novo_estado]
-    a2.estados_iniciais[0] = novo_estado
-
-    # Copia as transições de a1 e a2 para o novo autômato
-    for estado, transicoes_estado in a1.transicoes.items():
-        transicoes[estado] = transicoes_estado.copy()
-   
-    for estado, transicoes_estado in a2.transicoes.items():
-        transicoes[estado] = transicoes_estado.copy()
-
-    # Atualiza a primeira transição das transições para ter o estado final de a1 como estado inicial de a2
-    for estado in transicoes:
-        for simbolo in transicoes[estado]:
-            if transicoes[estado][simbolo] == [a1.estados_finais[0]]:  # Testa se o proximo estado é o estado final de a1
-                transicoes[estado][simbolo] = [a2.estados_iniciais[0]]  # Atualiza o estado final de a1 para o estado inicial de a2
-
-    return AFND(estados_iniciais, conjunto_estados, estados_finais, transicoes)
-##############################################################################################################
-##############################################################################################################
-def concatenacao2(a1, a2):
+#############################################################################################################
+def concatenacao(a1, a2):
     estados_iniciais = a1.estados_iniciais  # Estado inicial de a1 como novo estado
     estados_finais = a2.estados_finais  # Estado final de a2 como novo estado
     conjunto_estados = a1.conjunto_estados + a2.conjunto_estados    #adiciona os estados de a1 e a2 no conjunto de estados
-    print_afdn(a1)
 
     transicoes = {} #cria um dicionario para as transicoes
     for estado, transicoes_estado in a1.transicoes.items():
@@ -157,39 +113,40 @@ def concatenacao2(a1, a2):
         transicoes[estado] = transicoes_estado.copy()   #Copia as transições de a2 para o novo autômato
 
     if(len(a1.estados_finais) > 1): #se o estado final de a1 for maior que 1 significa que a1 é uma união de estados
-        print("len(a1.estados_finais) > 1")
+        print("len(a1.estados_finais) > 1 CONC")
         for estado in a1.estados_finais:   #percorre os estados finais de a1
-            transicoes[estado] = {} #cria um dicionario para as transições que antes nao existia
-            transicoes[estado]["ε"] = a2.estados_iniciais[0] #adiciona a transição vazia para o estado inicial de a2 à lista de transições
+            # Se transicoes[estado]["ε"] já existir, então adicione a transição vazia para o estado inicial de a2 à lista de transições
+            if "ε" in transicoes[estado]: 
+                transicoes[estado]["ε"].append(a2.estados_iniciais[0])
+            else:
+                transicoes[estado] = {} #cria um dicionario para as transições que antes nao existia
+                transicoes[estado]["ε"] = [a2.estados_iniciais[0]] #adiciona a transição vazia para o estado inicial de a2 à lista de transições
 
+            
     elif(a2.estados_iniciais[0] == a2.estados_finais[0]): #se o estado inicial de a2 for igual ao estado final de a2 significa que *(Kleene) foi usado
         print("a2.estados_iniciais[0] == a2.estados_finais[0]")
         transicoes[a1.estados_finais[0]] = {}   #sobreescreve o estado final de a1
-        transicoes[a1.estados_finais[0]]["ε"] = a2.estados_iniciais[0]
+        transicoes[a1.estados_finais[0]]["ε"] = [a2.estados_iniciais[0]]
 
-    elif(a1.estados_finais[0] == 's0'): #se o estado final de a1 for = s0 significa que a1 tem fecho de kleene
+    elif(a1.estados_finais[0][0] == 'K'): #se o estado final de a1 for = s0 significa que a1 tem fecho de kleene
         print("a1.estados_finais[0] == 's0'")
         transicoes[a1.estados_finais[0]]["ε"] = [transicoes[a1.estados_finais[0]]["ε"]]
         transicoes[a1.estados_finais[0]]["ε"].append(a2.estados_iniciais[0]) #Adiciona nova transição vai para o estado inicial de a2
     else:
         transicoes[a1.estados_finais[0]] = {}   #sobreescreve o estado final de a1
-        transicoes[a1.estados_finais[0]]["ε"] = a2.estados_iniciais[0]
+        transicoes[a1.estados_finais[0]]["ε"] = [a2.estados_iniciais[0]]
     return AFND(estados_iniciais, conjunto_estados, estados_finais, transicoes)
 ##############################################################################################################
 def uniao_afnd(a1, a2):
-    novo_estado_inicial = 's0'  # Novo estado inicial
+    novo_estado_inicial = "U" + str(random.randint(1, 99))
     estados_finais = a1.estados_finais + a2.estados_finais  # Combine os estados finais de a1 e a2
 
-    conjunto_estados = a1.conjunto_estados + a2.conjunto_estados
-
+    conjunto_estados = list(set([novo_estado_inicial] + a1.conjunto_estados + a2.conjunto_estados))  # Inclui o novo estado inicial e une os conjuntos de estados
     transicoes = {}
     transicoes[novo_estado_inicial] = {'ε': [a1.estados_iniciais[0], a2.estados_iniciais[0]]}  # Transições vazias do estado inicial para os iniciais de a1 e a2
 
     for estado, transicoes_estado in a1.transicoes.items():
-        if estado in transicoes:
-            transicoes[estado].update(transicoes_estado)
-        else:
-            transicoes[estado] = transicoes_estado.copy()
+        transicoes[estado] = transicoes_estado.copy()
 
     for estado, transicoes_estado in a2.transicoes.items():
         if estado in transicoes:
@@ -200,15 +157,24 @@ def uniao_afnd(a1, a2):
     return AFND([novo_estado_inicial], conjunto_estados, estados_finais, transicoes)
 ##############################################################################################################
 def fecho_kleene(a):
-    novo_estado = "s0"
+    novo_estado = "K" + str(random.randint(1, 99))
     estados_iniciais = [novo_estado]
     conjunto_estados = [novo_estado] + a.conjunto_estados
     
     transicoes = {}
-    transicoes[novo_estado] = {}
-    transicoes[novo_estado]["ε"] = a.estados_iniciais[0]
-    transicoes[a.estados_finais[0]] = {}
-    transicoes[a.estados_finais[0]]["ε"] = novo_estado
+
+    if(len(a.estados_finais) > 1): #se o estado final de a1 for maior que 1 significa que a1 é uma união de estados
+        print("len(a1.estados_finais) > 1")
+        for estado in a.estados_finais:   #percorre os estados finais de a1
+            transicoes[estado] = {} #cria um dicionario para as transições que antes nao existia
+            transicoes[estado]["ε"] = [novo_estado] #adiciona a transição vazia para o estado inicial de a2 à lista de transições
+        transicoes[novo_estado] = {}
+        transicoes[novo_estado]["ε"] = [a.estados_iniciais[0]]
+    else:
+        transicoes[novo_estado] = {}
+        transicoes[novo_estado]["ε"] = [a.estados_iniciais[0]]
+        transicoes[a.estados_finais[0]] = {}
+        transicoes[a.estados_finais[0]]["ε"] = [novo_estado]
 
     estados_finais = [novo_estado]
 
@@ -217,7 +183,6 @@ def fecho_kleene(a):
         transicoes[estado] = transicoes_estado.copy()
 
     return AFND(estados_iniciais, conjunto_estados, estados_finais, transicoes)
-
 ##############################################################################################################
 conjunto_estados_utilizados = set()
 
@@ -258,7 +223,7 @@ def gerar_automato(arvore):
             # print_afdn(afdn_esquerdo)
             afdn_direito = gerar_automato(arvore.children[1])
             # print_afdn(afdn_direito)
-            return concatenacao2(afdn_esquerdo,afdn_direito)
+            return concatenacao(afdn_esquerdo,afdn_direito)
         elif arvore.value == "OU":
             afdn_esquerdo = gerar_automato(arvore.children[0])
             afdn_direito = gerar_automato(arvore.children[1])
@@ -316,6 +281,61 @@ def print_tree(node, level=0):
     else:
         print("  " * level + node)
 
+def automaton_to_jff(automaton, filename):
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_directory, filename)
+
+    state_id_map = {}  # To store unique state IDs
+
+    def get_state_id(state):
+        if state not in state_id_map:
+            state_id_map[state] = len(state_id_map)
+        return state_id_map[state]
+
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write('<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n')
+        file.write('<!--Created with JFLAP 7.1.-->\n')
+        file.write('<structure>\n')
+        file.write('\t<type>fa</type>\n')
+        file.write('\t<automaton>\n')
+
+        # Write the states
+        for state in automaton.conjunto_estados:
+            state_id = get_state_id(state)
+
+            x_position = state_id * 50
+            y_position = state_id * 50
+
+            file.write(f'\t\t<state id="{state_id}" name="{state}">\n')
+            file.write(f'\t\t\t<x>{x_position}</x>\n')
+            file.write(f'\t\t\t<y>{y_position}</y>\n')
+
+            if state in automaton.estados_iniciais:
+                file.write('\t\t\t<initial/>\n')
+            if state in automaton.estados_finais:
+                file.write('\t\t\t<final/>\n')
+
+            file.write('\t\t</state>\n')
+
+        # Write the transitions
+        for source, transitions in automaton.transicoes.items():
+            for symbol, targets in transitions.items():
+                for target in targets:
+                    if source in automaton.conjunto_estados and target in automaton.conjunto_estados:
+                        file.write('\t\t<transition>\n')
+                        file.write(f'\t\t\t<from>{get_state_id(source)}</from>\n')
+                        file.write(f'\t\t\t<to>{get_state_id(target)}</to>\n')
+                        if symbol == 'ε':
+                            file.write('\t\t\t<read/>\n')
+                        else:
+                            file.write(f'\t\t\t<read>{symbol}</read>\n')
+                        file.write('\t\t</transition>\n')
+                    else:
+                        print(f"Warning: Transition is defined to non-existent state {source} or {target}!")
+
+        file.write('\t</automaton>\n')
+        file.write('</structure>\n')
+
 def main():
     expressao, nome_saida_arquivo = ler_expressao_do_arquivo()
     if expressao:
@@ -327,6 +347,8 @@ def main():
             # Remove a extensão .txt do nome_saida_arquivo antes de salvar
             nome_saida_arquivo = os.path.splitext(nome_saida_arquivo)[0]
             salvar_afdn_para_arquivo(automato, nome_saida_arquivo)
-
+           # Assuming you have an automaton named "automaton" and a desired filename "automaton.jff", you can call the function as follows:
+            nome_saida_arquivo = os.path.splitext(nome_saida_arquivo)[0]
+            automaton_to_jff(automato, nome_saida_arquivo + ".jff")
 if __name__ == "__main__":
     main()
